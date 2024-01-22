cd23b5778517113b088bd6ee1ae75301bf43c5db
Faruk Önder <Farukonder@icloud.com>
2024-01-23 01:15:28 +0300

    Update:: V2 Portfolio

---
+++ script.js
@@ -0,0 +1,31 @@
+/**
+ * @copyright codewithsadee 2023
+ * @author Sadee <codewithsadee24@gmail.com>
+ */
+"use strict";
+
+/**
+ * Light and Dark Mode
+*/
+
+const /** {NodeElement} */ $themeBtn = document.querySelector("[data-theme-btn]");
+const /** {NodeElement} */ $HTML = document.documentElement;
+let /** {Boolean | String} */ isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (sessionStorage.getItem("theme")) {
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+}
+
+else {
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+    /** sessionStorage.setItem("theme", $HTML.dataset.theme); */
+}
+
+const changeTheme = () => {
+    $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" :
+    "light";
+    sessionStorage.setItem("theme", $HTML.dataset.theme);
+}
+
+$themeBtn.addEventListener("click", changeTheme);
+/** window.addEventListener("load", () => $themeBtn.addEventListener("click")); */
