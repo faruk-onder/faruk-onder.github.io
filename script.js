7f4a48968c1f2c194d619ebbd60acc12a83571c6
Faruk Önder <Farukonder@icloud.com>
2024-01-23 03:08:14 +0300

    Update v2

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ if (sessionStorage.getItem("theme")) {
 
 else {
     $HTML.dataset.theme = isDark ? "dark" : "light";
-    /** sessionStorage.setItem("theme", $HTML.dataset.theme); */
 }
 
 const changeTheme = () => {
@@ -28,4 +27,25 @@ const changeTheme = () => {
 }
 
 $themeBtn.addEventListener("click", changeTheme);
-/** window.addEventListener("load", () => $themeBtn.addEventListener("click")); */
+
+/*
+ * TAB
+ */
+
+const /** {NodeList} */ $tabBtn = document.querySelectorAll("[data-tab-btn]");
+let /** {NodeElement} */ [lastActiveTab] = document.querySelectorAll("[data-tab-content]");
+let /** {NodeElement} */ [lastActiveTabBtn] = $tabBtn;
+
+$tabBtn.forEach(item => {
+    item.addEventListener("click", function(){
+        lastActiveTab.classList.remove("active");
+        lastActiveTabBtn.classList.remove("active");
+        
+        const /** {NodeElement} */ $tabContent = document.querySelector('[data-tab-content="${item.dataset.tabBtn}"]');
+        $tabContent.classList.add("active");
+        this.classList.add("active");
+        
+        lastActiveTab = $tabContent;
+        lastActiveTabBtn = this;
+    });
+});
